010361474b6bea9a25f94ee1a1b44c0921581349
Hao Tian Guan <haotian.guan@ontariotechu.net>
2021-12-07 13:07:36 -0500

    Added Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+// References
+// www.youtube.com/watch?v=dwcT-Dch0bA
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character 
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        jump = false;
+    }
+}
