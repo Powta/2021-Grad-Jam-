1f23c0e6f35c8d955b84c3ea9808d2450aa5b336
Isaiah Noah <isaiahnoah.santos@ontariotechu.net>
2021-12-09 18:51:08 -0500

    Donut enemy movement done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,8 +8,50 @@ public class Enemy : MonoBehaviour
 
     public GameObject deathEffect;
 
+    //movement variables
+    public float minPos;
+    public float maxPos;
+    private Rigidbody2D myRb;
+    private bool isGoingLeft = true;
+    public float moveSpeed;
+
+    private void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (isGoingLeft)
+        {
+            if (transform.position.x >= minPos)
+            {
+                myRb.velocity = new Vector2(-moveSpeed, myRb.velocity.y);
+            }
+            else
+            {
+                isGoingLeft = false;
+            }
+        }
+
+        else
+        {
+            if (transform.position.x <= maxPos)
+            {
+                myRb.velocity = new Vector2(moveSpeed, myRb.velocity.y);//move right
+            }
+            else
+            {
+                isGoingLeft = true;
+            }
+        }
+    }
     // Start is called before the first frame update
-   public void TakeDamage (int damage)
+    public void TakeDamage (int damage)
     {
         health -= damage;
 
